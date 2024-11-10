93839ca5fecb26ec587c3312a408d849aad664cd
Aum Patel <aumpatel2930@gmail.com>
2024-11-09 23:56:38 -0500

    Add files via upload

---
+++ Practice Set - 1/script.js
@@ -0,0 +1,13 @@
+// Q1 Create a const object called "product" to store information shown in the picture (pen in the picture in that the details of the name of the pen and the mrp of it).
+
+
+const Product = {
+    Item_Type : "Pen",
+    Item_Name : "Parker Jotter Standard CT Ball Pen",
+    Item_Color : "Black",
+    Item_MRP : "rs270",
+    Item_Ratings : "4/5",
+}
+
+console.log(Product, typeof(Product));
+console.log(typeof(Product));
---
+++ Practice Set - 1/script2.js
@@ -0,0 +1,14 @@
+// Q2 Create a const object "Profile" to store information shown in the picture (picture of insta id and some basics details about the id like profile name and the bio).
+
+
+const Profile =  {
+    profile_name : "Shradha Khapra",
+    profile_id : "@shradhakhapra",
+    profile_posts : 195,
+    profile_followers : "569k",
+    profile_following : 4,
+    profile_bio : "To educate someone is the highest privilege."
+}
+
+console.log(Profile);
+console.log(typeof(Profile));
