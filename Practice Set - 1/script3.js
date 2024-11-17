23336a1939bca858fd501aa9a7890210e1f77a93
Aum Patel <aumpatel2930@gmail.com>
2024-11-17 01:08:50 -0500

    Delete Practice Set - 1 directory

--- Practice Set - 1/script3.js
+++
@@ -1,7 +0,0 @@
-// Q3 Create a variable of type string and try to add a number to it 
-// Q4 Use type of operator to find the data type of the string in last question.
-
-
-let str = "33";
-console.log(str);
-console.log(typeof(str));
